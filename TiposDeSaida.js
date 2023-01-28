6c624534b2df6973d25bfcb4d44d60f6513d6076
Vitor Oliveira <victorskw89@gmail.com>
2023-01-28 20:53:23 -0300

    Add files via upload

---
+++ TiposDeSaida.js
@@ -0,0 +1,10 @@
+     // Tipos de saída usados em testes
+     // innerHTML
+     document.getElementById("#texto").innerHTML ="Aqui vai a mensagem ";
+     
+     // document.write ( escrever no documento )
+     document.write("Hello World");
+     
+     // alert apresenta uma caixa na tela 
+     alert("Atenção!");
+     
---
+++ var.js
@@ -0,0 +1,75 @@
+      // declarando variáveis 
+      let aguaCardinal = "Câncer ♋ ";
+      let terraCardinal = "Capricórnio ♑";
+      let fogoCardinal = "Áries ♈";
+      let arCardinal = "Libra ♎";
+      
+      let aguaFixo = "Escorpião ♏";
+      let terraFixo = "Touro ♉";
+      let fogoFixo = "Leão ♌";
+      let arFixo = "Aquário ♒";
+      
+      let aguaMutavel = "Peixes";
+      let terraMutavel = "virgem";
+      let fogoMutavel = "Sagitário";
+
+      console.log("Seu signo Solar é  " + aguaCardinal + "Sua Lua está em " + terraFixo + " E seu Ascendente é " + arFixo);
+      
+      let sol = "Sol";
+      let marte = "Marte";
+      let venus = "Vênus";
+      let mercurio = "Mercúrio";
+      let nodoNorte = "Nodo Norte";
+      
+      console.log("Você tem " + sol + ", " + marte + ", " + venus + ", " + mercurio + " & " + nodoNorte + ", Na casa Astrologica 6, potencializando os assuntos gerais desta área");
+      
+      
+      let nomeDaRede = "Oi Ponto Fibra";
+      let senha = "Senha Do Wi-Fi :abraCadabra123";
+      let sinal = "Potência do sinal Forte";
+      let frequencia = "Frequência 2,4 Ghz";
+      let seguranca = "Segurança WPA/WPA2-Personal";
+      let mac = "Endereço MAC ca:f6:89:01:44:e8";
+      let ip = "Endereço IP 192.350.1.115";
+      let gateway = "Gateway 198.909.1.1";
+      let mascara = "Mascara De Sub Rede 255.255.00";
+      let dns = "DNS 192.168.2.2 ";
+      
+      console.log(nomeDaRede + " " + senha + " " + sinal + " " + frequencia + " " + seguranca + " " + mac);
+      
+    var a, b, c;
+    a = 100;
+    b = 200;
+    c = a + b;
+    
+    console.log(c);
+    
+    var leite, arroz, biscoito, tempero, miojo, total;
+    leite = 5;
+    arroz = 20;
+    biscoito = 7;
+    tempero = 3;
+    miojo = 4;
+    total = leite + arroz + biscoito + tempero + miojo;
+    
+    console.log("O total da compra no Supermercado Deu: R$" + total + "  Reais");
+      
+    var luz, gas, agua, comida, aluguel, internet, totalgeral;
+    luz = 200;
+    gas = 150;
+    agua = 60;
+    comida = 100;
+    aluguel = 600;
+    internet = 70;
+    totalgeral = luz + gas + agua + comida + aluguel + internet;
+    
+    console.log("O total de gastos do mês foi : R$" + totalgeral + "Reais");
+    
+    // const ( constantes são sempre o valor que você determina e não mudam por nada)
+    const carroMarca = "Ferrari";
+    const carroBanco = "Couro";
+    const carroCor = "Vermelho";
+    const vidroFume = "Vidros Fume";
+    const carroAltura = "2 metros";
+    
+    
